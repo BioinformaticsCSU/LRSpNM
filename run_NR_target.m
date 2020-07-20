85a0ea6ed414bfd61a390e1eba1fce61e7356f9d
XingyuLiao <46807118+XingyuLiao@users.noreply.github.com>
2020-07-20 11:01:10 +0800

    Update run_NR_target.m

--- run_NR_target.m
+++ run_NR_target.m
@@ -1,13 +1,16 @@
 clear all
 clc
 
-% LOAD WANG Dataset
-path='data\';
+% LOAD Dataset
+path='Data\';
 % the different datasets
 datasets={'e','ic','gpcr','nr'};
-ds = 4;
+ds = 4; % Nuclear Receptor dataset
 [Y,Sd,St,~,~]=getdata(path,datasets{ds});
 Y = Y';
+
+% LOAD WANG Dataset
+% load 'Data/WANG'
 
 % CV parameters
 m = 5;  % number of n-fold experiments (repetitions)
@@ -29,15 +32,28 @@ c1 = 0.01;
 c2 = 0.0001;
 K = 7;
 
+% fixed seeds (for reproducibility)
+% cv_drug setting: all drugs (drug prediction)
+if strcmp(cv_setting, 'cv_drug')
+    seeds = [6381  1535  9727  9129  6802 ]; 
+
+% cv_target: all targets (target prediction)
+else 
+    seeds = [8763  7952  402  9781  9541 ];  
+end
+
 % cross validation (m repetitions of n-fold experiments)
 AUPR = zeros(1,m);
 for k=1:m
+    seed = seeds(k);
     [num_drugs,num_targets] = size(Y);
     if strcmp(cv_setting,'cv_drug')
         len = num_drugs;
     else
         len = num_targets;
     end
+    rng('default')
+    rng(seed);
     rand_ind = randperm(len);
     
     AUPRs = zeros(1,n);
